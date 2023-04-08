5d3e9491274d5ed7a8500b2efb5d6768bab31641
Luciano Germani <lucianogermani93@gmail.com>
2023-04-08 21:39:07 +0200

    Fix: Remove checkrole middleware from auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,11 @@
 const authController                = require('../controllers/AuthController');
 const verifyToken                   = require('../middlewares/verifyToken');
-const checkRole                     = require('../middlewares/checkRole');
 const router                        = require('express').Router();
 
 //Routes
 router.post('/auth/userSignIn', authController.registerUser);
 router.post('/auth/userLogin', authController.login);
-router.get('/auth/profile', verifyToken,checkRole, authController.profile);
+router.get('/auth/profile', verifyToken, authController.profile);
 
 
 module.exports = router;
